401afb4c53e5a38fea66f2773750c0e61da7e383
guide <guide@airgradient.com>
2025-05-09 11:28:07 +0700

    First working commit for v3.x — implementation verified and functional

---
+++ main/MaxConfig 2.h
@@ -0,0 +1,73 @@
+#ifndef MAX_CONFIG_H
+#define MAX_CONFIG_H
+
+#include "soc/gpio_num.h"
+
+#define MAX_PAYLOAD_CACHE 100
+
+#define MILLIS() ((uint32_t)(esp_timer_get_time() / 1000))
+
+#define MAX_BOARD_2XX 1
+#define MAX_BOARD_3XX 2
+
+#define BOARD_VERSION MAX_BOARD_2XX
+
+#if BOARD_VERSION == MAX_BOARD_3XX
+constexpr gpio_num_t EN_CO2 = GPIO_NUM_5;
+constexpr gpio_num_t EN_PMS = GPIO_NUM_3;
+constexpr gpio_num_t EN_NO2 = GPIO_NUM_4;
+constexpr gpio_num_t IO_WDT = GPIO_NUM_2;
+constexpr gpio_num_t EN_CE_CARD = GPIO_NUM_15;
+constexpr gpio_num_t IO_CE_POWER = GPIO_NUM_23;
+constexpr gpio_num_t IO_IIC_RESET = GPIO_NUM_21;
+constexpr gpio_num_t IO_LED_INDICATOR = GPIO_NUM_10;
+#else // 2XX
+constexpr gpio_num_t EN_CO2 = GPIO_NUM_15;
+constexpr gpio_num_t EN_PM1 = GPIO_NUM_3;
+constexpr gpio_num_t EN_PM2 = GPIO_NUM_11;
+constexpr gpio_num_t IO_WDT = GPIO_NUM_2;
+constexpr gpio_num_t EN_CE_CARD = GPIO_NUM_22;
+constexpr gpio_num_t IO_CE_POWER = GPIO_NUM_23;
+constexpr gpio_num_t IO_IIC_RESET = GPIO_NUM_21;
+constexpr gpio_num_t IO_LED_INDICATOR = GPIO_NUM_10;
+#endif
+
+#define UART_RX_SUNLIGHT 0
+#define UART_TX_SUNLIGHT 1
+#define UART_BAUD_SUNLIGHT 9600
+#define UART_PORT_SUNLIGHT 0
+
+#define UART_RX_CE_CARD 17
+#define UART_TX_CE_CARD 16
+#define UART_BAUD_CE_CARD 115200
+#define UART_BAUD_PORT_CE_CARD 1
+
+#define I2C_MASTER_SCL_IO 6
+#define I2C_MASTER_SDA_IO 7
+#define I2C_MASTER_FREQ_HZ 100000
+#define I2C_MASTER_PORT 0
+
+#define DEFAULT_INVALID_TEMPERATURE -1000
+#define DEFAULT_INVALID_HUMIDITY -1
+#define DEFAULT_INVALID_PM -1
+#define DEFAULT_INVALID_CO2 -1
+#define DEFAULT_INVALID_TVOC -1
+#define DEFAULT_INVALID_NOX -1
+#define DEFAULT_INVALID_VOLT -1
+
+#define IS_PM_VALID(val) (val >= 0)
+#define IS_TEMPERATURE_VALID(val) ((val >= -40) && (val <= 125))
+#define IS_HUMIDITY_VALID(val) ((val >= 0) && (val <= 100))
+#define IS_CO2_VALID(val) ((val >= 0) && (val <= 10000))
+#define IS_TVOC_VALID(val) (val >= 0)
+#define IS_NOX_VALID(val) (val >= 0)
+#define IS_VOLT_VALID(val) (val >= 0)
+
+#define TRANSMIT_MEASUREMENTS_CYCLES 3
+#define MEASURE_CYCLE_INTERVAL_SECONDS 180
+#define FIRMWARE_UPDATE_CHECK_CYCLES 30 // 1 hour
+
+#define DEFAULT_MEASURE_ITERATION_COUNT 20
+#define DEFAULT_MEASURE_INTERVAL_MS_PER_ITERATION 2000
+
+#endif
---
+++ main/StatusLed 2.cpp
@@ -0,0 +1,115 @@
+#include "StatusLed.h"
+#include "driver/gpio.h"
+#include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
+#include "freertos/task.h"
+#include "esp_timer.h"
+#include "esp_log.h"
+
+#include "portmacro.h"
+
+#define MILLIS() ((uint32_t)(esp_timer_get_time() / 1000))
+
+StatusLed::StatusLed(gpio_num_t ioLed) : _ioLed(ioLed) {}
+
+StatusLed::~StatusLed() {
+  // stop task
+  stop();
+}
+
+esp_err_t StatusLed::start() {
+  BaseType_t result;
+  result = xTaskCreate(&StatusLed::_start, "StatusLed", 1024, this, 5, &_taskHandle);
+  if (result != pdPASS) {
+    ESP_LOGE(TAG, "Failed start status led task");
+    return ESP_FAIL;
+  }
+
+  gpio_hold_dis(_ioLed);
+  gpio_reset_pin(_ioLed);
+  gpio_set_direction(_ioLed, GPIO_MODE_OUTPUT);
+  gpio_set_level(_ioLed, 0);
+
+  ESP_LOGD(TAG, "Success start status led task");
+  return ESP_OK;
+}
+
+void StatusLed::stop() {
+  if (_isRunning) {
+    vTaskDelete(_taskHandle);
+  }
+}
+
+void StatusLed::disable() {
+  gpio_set_level(_ioLed, 0);
+  gpio_hold_en(_ioLed);
+}
+
+void StatusLed::set(Mode mode, int durationMs, int intervalMs) {
+  _lastDuration = durationMs;
+  _lastMode = mode;
+  _lastInterval = intervalMs;
+  // Notify task there's a new led event
+  xTaskNotifyGive(_taskHandle);
+}
+
+void StatusLed::_start(void *params) {
+  StatusLed *pLed = static_cast<StatusLed *>(params);
+
+  // This variable act as how long to wait for notification
+  // And its also act as blink interval
+  TickType_t waitNotificationTick = portMAX_DELAY;
+
+  // waitNotificationTick = 100 / portTICK_PERIOD_MS;
+  uint32_t notif;
+
+  Mode mode;
+  int duration = 0;
+  int interval;
+
+  int lastLedLevel = 0;
+  int blinkStartTime = 0;
+
+  while (1) {
+    notif = ulTaskNotifyTake(pdTRUE, waitNotificationTick);
+    if (notif > 0) {
+      // New status led event received
+      mode = pLed->_lastMode;
+      duration = pLed->_lastDuration;
+      interval = pLed->_lastInterval;
+
+      // Handle start of the event based on each mode
+      // For On and Off, no other action required, wait for event with blocking task
+      switch (mode) {
+      case Off:
+        gpio_set_level(pLed->_ioLed, 0);
+        waitNotificationTick = portMAX_DELAY;
+        break;
+      case On:
+        gpio_set_level(pLed->_ioLed, 1);
+        waitNotificationTick = portMAX_DELAY;
+        break;
+      case Blink:
+        gpio_set_level(pLed->_ioLed, 1); // turn on first
+        waitNotificationTick = interval / portTICK_PERIOD_MS;
+        blinkStartTime = MILLIS();
+        break;
+      }
+
+      continue;
+    }
+
+    // The rest is for blink mode
+
+    // Check if duration is UP and duration is not set to forever (0)
+    if ((MILLIS() - blinkStartTime) > duration && duration > 0) {
+      gpio_set_level(pLed->_ioLed, 0);
+      duration = 0;
+      waitNotificationTick = portMAX_DELAY;
+      continue;
+    }
+
+    lastLedLevel = !lastLedLevel;
+    gpio_set_level(pLed->_ioLed, lastLedLevel);
+  }
+}
